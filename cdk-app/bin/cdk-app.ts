eb456bf91ea76ca2e97c2d3d7f8b59e625b5f5c8
Miguel A. Calles MBA <44813512+miguel-a-calles-mba@users.noreply.github.com>
2023-05-26 14:43:55 -0700

    Chapter 2

--- cdk-app/bin/cdk-app.ts
+++ cdk-app/bin/cdk-app.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkAppStack } from '../lib/cdk-app-stack';
+import { WebsiteStack } from '../lib/website-stack';
 
 const app = new cdk.App();
-new CdkAppStack(app, 'CdkAppStack', {
+new WebsiteStack(app, 'WebsiteStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
